90faee254a406a6d1371dc5889471366a2ee0f1d
Chong Wang <1147833662@qq.com>
2024-01-24 10:28:05 +0800

    Update train_codegpt.py

--- script/train/train_codegpt.py
+++ script/train/train_codegpt.py
@@ -17,8 +17,8 @@ from coder.constants import PLM_LSP_POINT
 if __name__ == '__main__':
     MODEL = "microsoft/CodeGPT-small-py"
 
-    LSP_MODE = "nolsp"
-    # LSP_MODE = "lsp"
+    # LSP_MODE = "nolsp"
+    LSP_MODE = "lsp"
 
     TIMESTAMP = time.strftime("%y%m%d-%H%M%S", time.localtime())
     DIR = f"models-{LSP_MODE}/{MODEL.split('/')[-1]}-{str(TIMESTAMP)}"
